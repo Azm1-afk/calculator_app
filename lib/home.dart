549184c73f033c7894aabe778fb6bb6f063c0086
Abdullahil Azmi <abdullahilazmi@gmail.com>
2025-05-30 20:32:19 +0600

    Got some logic behind it. Using GPT to understand it better.

--- lib/home.dart
+++ lib/home.dart
@@ -1,68 +1,108 @@
 import 'package:flutter/material.dart';
 import 'widgets/num_button.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TextEditingController inputControl = TextEditingController();
+  State<Home> createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  String _input = '';
+  String _result = '';
+  String _operator = '';
+  double? _firstOperand;
+
+  void _onButtonPress(String value) {
+    setState(() {
+      if (value == '+' || value == '-' || value == '*' || value == '/') {
+        _firstOperand = double.tryParse(_input);
+        _operator = value;
+        _input = ''; // clear input for second operand
+      } else if (value == '=') {
+        double? secondOperand = double.tryParse(_input);
+        if (_firstOperand != null && secondOperand != null) {
+          switch (_operator) {
+            case '+':
+              _result = (_firstOperand! + secondOperand).toString();
+              break;
+            case '-':
+              _result = (_firstOperand! - secondOperand).toString();
+              break;
+            case '*':
+              _result = (_firstOperand! * secondOperand).toString();
+              break;
+            case '/':
+              _result = secondOperand != 0
+                  ? (_firstOperand! / secondOperand).toString()
+                  : 'Error';
+              break;
+          }
+        }
+      } else if (value == 'C') {
+        _input = '';
+        _result = '';
+        _firstOperand = null;
+        _operator = '';
+      } else {
+        _input += value;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final List<String> buttons = [
-      '7',
-      '8',
-      '9',
-      '/',
-      '4',
-      '5',
-      '6',
-      '*',
-      '1',
-      '2',
-      '3',
-      '-',
-      '0',
-      '.',
-      '=',
-      '+',
+      '7', '8', '9', '/',
+      '4', '5', '6', '*',
+      '1', '2', '3', '-',
+      'C', '0', '=', '+',
     ];
 
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Calculator'),
         backgroundColor: Colors.grey[200],
       ),
-
       body: Column(
         children: [
+          const SizedBox(height: 20),
           Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(7),
-              color: Colors.black,
+              color: Colors.white,
             ),
             child: Padding(
               padding: const EdgeInsets.all(25.0),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                cursorColor: Colors.white,
-                textAlign: TextAlign.end,
-                style: TextStyle(color: Colors.white, fontSize: 35),
-
-                decoration: InputDecoration(border: InputBorder.none),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    _input,
+                    style: TextStyle(color: Colors.black, fontSize: 35),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    _result,
+                    style: TextStyle(color: Colors.green, fontSize: 28),
+                  ),
+                ],
               ),
             ),
           ),
-
-          SizedBox(height: 20),
-
+          const SizedBox(height: 20),
           Expanded(
             child: GridView.count(
               crossAxisCount: 4,
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
               padding: const EdgeInsets.all(16.0),
-              children: buttons.map((value) => NumButton(text: value)).toList(),
+              children: buttons.map((value) => NumButton(
+                text: value,
+                onPressed: () => _onButtonPress(value),
+              )).toList(),
             ),
           ),
         ],
--- lib/widgets/num_button.dart
+++ lib/widgets/num_button.dart
@@ -2,30 +2,37 @@ import 'package:flutter/material.dart';
 
 class NumButton extends StatelessWidget {
   final String text;
+  final VoidCallback onPressed;
 
   const NumButton({
     Key? key,
     required this.text,
+    required this.onPressed
   }) : super(key: key);
 
-  @override
+
+
+  @override   // THE BELOW CODE IS THE STRUCTURAL/PHYSICAL ELEMENTS
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(4),
-      decoration: BoxDecoration(
-        color: Colors.blue[100],
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      width: 80,
-      height: 80,
-      child: Center(
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        margin: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: Colors.blue[100],
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        width: 80,
+        height: 80,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
